199c7e1964800c2794310e3bb2515b03f1e2e670
Juan Jose Valencia Ceballos <juan.valencia@ceiba.com.co>
2015-10-15 16:58:26 -0500

    Anexo directiva y filtro

---
+++ app/scripts/directives/directivaMaximoCaracteres.js
@@ -0,0 +1,23 @@
+
+function maximoCaracteres() {
+  return {
+    require: 'ngModel',
+    link: function (scope, element, attrs, ngModelCtrl) {
+      var maxlength = Number(attrs.myMaxlength);
+      function fromUser(text) {
+          if (text.length > maxlength) {
+            var transformedInput = text.substring(0, maxlength);
+            ngModelCtrl.$setViewValue(transformedInput);
+            ngModelCtrl.$render();
+            return transformedInput;
+          }
+          return text;
+      }
+      ngModelCtrl.$parsers.push(fromUser);
+    }
+  };
+}
+
+
+angular.module('GestionDocumental')
+.directive('maximoCaracteres', maximoCaracteres);
---
+++ app/scripts/filters/filtroNumeros.js
@@ -0,0 +1,13 @@
+
+
+function soloNumeros(){
+  return function(input) {
+    return !isNaN(parseFloat(input)) && isFinite(input) ? input : input.substring(0, input.length-1);
+  };
+}
+
+
+
+
+angular.module('GestionDocumental')
+.filter('soloNumeros', soloNumeros);
